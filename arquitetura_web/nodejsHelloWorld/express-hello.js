6fec63b7cb24c0189c960482b68fbf97ecaf8543
Pedro Augusto <104510410+PedroAugusto2101@users.noreply.github.com>
2024-09-06 17:25:57 -0300

    Delete arquitetura_web/nodejsHelloWorld directory

--- arquitetura_web/nodejsHelloWorld/express-hello.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { request } = require('http');
-app = express();
-
-app.get("/hello", (request, response) => {
-    response.send("Hello World");
-});
-
-app.listen(3000);
