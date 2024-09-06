af5e80844afd897eb34d03b6edc9cf094a09e380
Pedro Augusto <104510410+PedroAugusto2101@users.noreply.github.com>
2024-09-06 15:58:27 -0300

    Add files via upload

---
+++ arquitetura_web/helloWorldSpring/src/test/java/com/example/helloWorldSpring/HelloWorldSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.helloWorldSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
